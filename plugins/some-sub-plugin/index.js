42585ead8df190b377cc1f9dd0baeb4272c3bbed
Florian Gyger <gygy1992@hotmail.com>
2020-02-28 10:47:16 +0100

    try linking the new node to the Mdx node as a child

--- plugins/some-sub-plugin/index.js
+++ plugins/some-sub-plugin/index.js
@@ -1,11 +1,11 @@
-module.exports = ({ actions, createNodeId, createContentDigest }) => {
-    console.log("Creating a new node");
+module.exports = ({ actions, createNodeId, createContentDigest, markdownNode }) => {
+    console.log(`Creating a new test node and link it to parent node: ${markdownNode.id}`);
     const { createNode } = actions;
     createNode({
         test: "testvalue",
 
         id: createNodeId("test"),
-        parent: null,
+        parent: markdownNode.id,
         children: [],
         internal: {
             contentDigest: createContentDigest("testvalue"),
